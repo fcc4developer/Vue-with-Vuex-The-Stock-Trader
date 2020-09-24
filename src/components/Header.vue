479f274207511541bff8c8e606f3554b0180df0c
fcc4developer <dragana.ilievska@gsix.me>
2020-09-25 01:02:43 +0200

    created components, setup routes, added header and navigation and created templates of stocks components.

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <router-link to="/">
+    <a class="navbar-brand">Stock Trader</a>
+  </router-link>
+
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <router-link to="/portfolio"  tag="li" class="nav-item" activeClass="active">
+        <a class="nav-link">Portfolio</a>
+      </router-link>
+      <router-link to="/stocks" tag="li" class="nav-item" activeClass="active">
+        <a class="nav-link">Stocks</a>
+      </router-link>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link">End Day</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Save & Load
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Save Data</a>
+          <a class="dropdown-item" href="#">Load Data</a>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
