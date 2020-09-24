ba23ca5f0a9be8a99a58a5d2986b11af371c8bd5
fcc4developer <dragana.ilievska@gsix.me>
2020-09-25 01:02:43 +0200

    created components, setup routes, added header and navigation and created templates for stocks components.

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Portfolio</h1>
+</template>
+<style scoped>
+  h1 {
+    padding: 30px
+  }
+</style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="card text-white">
+        <div class="card-header bg-info">
+            <h4 class="card-title pt-2">Name
+                <small class="card-text">(Price: PRICE)</small>
+            </h4>
+        </div>
+        <div class="card-body">
+            <div class="float-left">
+                <input type="number" class="form-control" placeholder="Quantity">
+            </div>
+            <div class="float-right">
+                <button class="btn btn-success">Buy</button>
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    padding: 30px
+  }
+</style>
