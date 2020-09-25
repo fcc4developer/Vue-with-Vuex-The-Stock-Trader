7e8a62afa58da5f3a9b1ef8237fbcb7d653bf44b
fcc4developer <dragana.ilievska@gsix.me>
2020-09-25 13:07:06 +0200

    Setting up the Vuex State Management, added Stocks module to Vuex.

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,21 +2,52 @@
   <div class="col-sm-6 col-md-4">
     <div class="card text-white">
         <div class="card-header bg-info">
-            <h4 class="card-title pt-2">Name
-                <small class="card-text">(Price: PRICE)</small>
+            <h4 class="card-title pt-2">{{ stock.name }}
+                <small class="card-text">(Price: {{ stock.price }})</small>
             </h4>
         </div>
         <div class="card-body">
             <div class="float-left">
-                <input type="number" class="form-control" placeholder="Quantity">
+                <input
+                  type="number"
+                  class="form-control"
+                  placeholder="Quantity"
+                  v-model="quantity"
+                  >
             </div>
             <div class="float-right">
-                <button class="btn btn-success">Buy</button>
+                <button
+                  class="btn btn-success"
+                  @click="buyStock"
+                  :disabled="quantity <= 0"
+                  >Buy</button>
             </div>
           </div>
       </div>
   </div>
 </template>
+
+<script>
+  export default {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity,
+        }
+        console.log(order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
 
 <style scoped>
   h1 {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 import App from './App.vue'
 import { routes } from './routes';
+import store from './store/store';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
@@ -14,5 +15,6 @@ const router = new VueRouter({
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
